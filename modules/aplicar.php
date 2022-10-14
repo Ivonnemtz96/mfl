72ccff529ef5d1f62f5634e798480fc165f72423
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-14 15:55:53 -0600

    aplicar terminado

--- modules/aplicar.php
+++ modules/aplicar.php
@@ -3,13 +3,13 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img" data-background="assets/images/bg/bg-12.png">
     <div class="section-logo-text">
-        <span class="title">SWORD</span>
+        <span class="title">MFL</span>
     </div>
     <div class="container-fluid">
         <div class="row justify-content-center align-items-end mb-30-none">
             <div class="col-xl-12 col-lg-12 text-center mb-30">
                 <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
-                    <h1 class="title">APPLY <span>NOW</span></h1>
+                    <h1 class="title">Aplica <span>ahora</span></h1>
                 </div>
             </div>
         </div>
@@ -18,8 +18,8 @@
 <div class="breadcrumb-area">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Apply Now</li>
+        <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Aplicar para MFL</li>
         </ol>
     </nav>
 </div>
@@ -31,33 +31,33 @@
     Start Account widget
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <section class="account-widget-section account-widget-section-two account-widget-section--style account-widget-section--style-two pt-120">
-    <div class="container">
+    <div class="container mb-120">
         <div class="row justify-content-center">
             <div class="col-xl-12">
                 <div class="account-widget-form-area">
                     <div class="row justify-content-center">
                         <div class="col-xl-10 col-lg-12 text-center">
                             <div class="section-header">
-                                <h2 class="section-title">For <span>Apply</span> Get Appointment <span>Today!</span></h2>
-                                <p>Fight School has specialized in martial arts since 1986 and has one of the most innovative most innovative programs in the nation.</p>
+                                <h2 class="section-title">Para <span>aplicar</span> a la MFL<span> regístrate ahora. </span></h2>
+                                <p>Derrota al enemigo que vive dentro de ti.</p>
                             </div>
                         </div>
                     </div>
                     <form class="account-widget-form">
                         <div class="row justify-content-center mb-20-none">
                             <div class="col-xl-6 col-lg-6 form-group">
-                                <label>Enter Name</label>
-                                <input type="text" class="form--control" placeholder="Micle Joncina">
+                                <label>Nombre completo</label>
+                                <input type="text" class="form--control" placeholder="Jorge Perez">
                             </div>
                             <div class="col-xl-6 col-lg-6 form-group">
-                                <label>Email Adress</label>
+                                <label>Correo</label>
                                 <input type="email" class="form--control" placeholder="example@email.com">
                             </div>
-                            <div class="col-xl-6 col-lg-6 form-group">
-                                <label>Phone Number</label>
-                                <input type="text" class="form--control" placeholder="+1 (800)-123-4567">
+                            <div class="col-xl-12 col-lg-12 form-group">
+                                <label>Teléfono</label>
+                                <input type="text" class="form--control" placeholder="624 123 4567">
                             </div>
-                            <div class="col-xl-6 col-lg-6 form-group">
+                            <!-- <div class="col-xl-6 col-lg-6 form-group">
                                 <label>Select your Training</label>
                                 <div class="account-widget-select">
                                     <select class="form--control">
@@ -66,13 +66,13 @@
                                         <option value="3">Personal Boxing</option>
                                     </select>
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="col-xl-12 form-group">
-                                <label>Write Message</label>
-                                <textarea class="form--control" placeholder="Start typing here"></textarea>
+                                <label>Escríbenos tus datos generales</label>
+                                <textarea class="form--control" placeholder="Edad, peso, estado y ciudad."></textarea>
                             </div>
                             <div class="col-xl-12 form-group text-center">
-                                <button type="submit" class="btn--base mt-20">Get Enrolled Now <i class="fas fa-arrow-right ml-2"></i></button>
+                                <button type="submit" class="btn--base mt-20">Aplicar a MFL<i class="fas fa-arrow-right ml-2"></i></button>
                             </div>
                         </div>
                     </form>
