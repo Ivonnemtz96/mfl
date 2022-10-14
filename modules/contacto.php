b0c639325314aa2a657a907a7255802cd862db21
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-10-14 15:47:22 -0600

    Contacto terminado

--- modules/contacto.php
+++ modules/contacto.php
@@ -1,7 +1,8 @@
 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Start Banner
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
-<section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img" data-background="assets/images/bg/bg-12.png">
+<section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img"
+    data-background="assets/images/bg/bg-12.png">
     <div class="section-logo-text">
         <span class="title">MFL</span>
     </div>
@@ -9,7 +10,7 @@
         <div class="row justify-content-center align-items-end mb-30-none">
             <div class="col-xl-12 col-lg-12 text-center mb-30">
                 <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
-                    <h1 class="title">CONTACT <span>US</span></h1>
+                    <h1 class="title">CONTÁC<span>TANOS</span></h1>
                 </div>
             </div>
         </div>
@@ -18,8 +19,8 @@
 <div class="breadcrumb-area">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
+            <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Contacto</li>
         </ol>
     </nav>
 </div>
@@ -30,18 +31,22 @@
     Start Contact-item
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <section class="contact-item-section ptb-120">
-    <div class="contact-element-one my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground" data-paroller-direction="vertical">
+    <div class="contact-element-one my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
         <img src="assets/images/element/element-1.png" alt="element">
     </div>
-    <div class="contact-element-two my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground" data-paroller-direction="vertical">
+    <div class="contact-element-two my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
         <img src="assets/images/element/element-2.png" alt="element">
     </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-xl-10 col-lg-12 text-center">
                 <div class="section-header">
-                    <h2 class="section-title">Ready to Get More <span>Information</span></h2>
-                    <p>There were twelve rules in all, and they specified that fights should be "a fair stand-up boxing match" in a 24-foot-square or similar ring. Rounds were three minutes with one-minute rest.</p>
+                    <h2 class="section-title">Si tienes alguna duda o pregunta <span>escríbenos</span></h2>
+                    <!-- <p>
+                        There were twelve rules in all, and they specified that fights should be "a fair stand-up boxing match" in a 24-foot-square or similar ring. Rounds were three minutes with one-minute rest.
+                    </p> -->
                 </div>
             </div>
         </div>
@@ -54,8 +59,9 @@
                         </div>
                     </div>
                     <div class="contact-content">
-                        <h4 class="title">OFFICE ADRESS</h4>
-                        <p>350 5th Ave, New York, NY 10118350 <br> <a href="https://www.google.com/maps/@23.7745978,90.4219535,15z" target="_blank">view map</a> </p>
+                        <h4 class="title">Dirección</h4>
+                        <p>San José del Cabo <br> <a href="https://www.google.com/maps/@23.0902874,-109.7091726"
+                                target="_blank">Ver mapa</a> </p>
                     </div>
                 </div>
             </div>
@@ -67,8 +73,8 @@
                         </div>
                     </div>
                     <div class="contact-content">
-                        <h4 class="title">TELEPHONE NUMBER</h4>
-                        <p>+1 (800)-123-4567 <br> <a href="tel:11234567">call now</a> </p>
+                        <h4 class="title">Teléfono</h4>
+                        <p>+52 123 456 7890<br> <a href="tel:+52 123 456 7890">Marcar ahora</a> </p>
                     </div>
                 </div>
             </div>
@@ -80,17 +86,15 @@
                         </div>
                     </div>
                     <div class="contact-content">
-                        <h4 class="title">SEND MAIL</h4>
-                        <p>example@email.com <br> <a href="mailto:">send now</a> </p>
+                        <h4 class="title">Correo</h4>
+                        <p>info@mexicanfightleague.com <br> <a href="mailto:info@mexicanfightleague.com">Enviar
+                                correo</a> </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    End Contact-item
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
 <!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Start Account widget
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
@@ -100,12 +104,17 @@
             <div class="col-xl-6 col-lg-6 mb-60">
                 <div class="account-widget-left">
                     <div class="section-header">
-                        <h2 class="section-title">Any Question? Feel
-                            Free to <span>Contact</span> with Us!</h2>
-                        <p>If you have any questions or need more information, please use the form below to send us a message There were twelve rules in all, and they specified that fights should be "a fair stand-up boxing match" in a 24-foot-square or similar ring. </p>
+                        <h2 class="section-title">¿Alguna pregunta?
+                            <span>Contáctanos</span>
+                        </h2>
+                        <p>
+
+                            Si tienes alguna duda o requiere más información, pueden escribirnos por medio de este
+                            formulario de contacto.
+                        </p>
                     </div>
                     <div class="banner-widget">
-                        <div class="banner-widget-wrapper">
+                        <!-- <div class="banner-widget-wrapper">
                             <div class="banner-widget-left">
                                 <div class="banner-widget-thumb">
                                     <img src="assets/images/element/element-9.png" alt="element">
@@ -116,14 +125,12 @@
                                     <p>Join over <span>4,000+</span> students</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                     <div class="contact-social-area">
-                        <span>FOLLOW US -</span>
+                        <span>SÍGUENOS EN NUESTRAS REDES-</span>
                         <ul class="contact-social">
                             <li><a href="#0"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#0"><i class="fab fa-google-plus-g"></i></a></li>
                             <li><a href="#0"><i class="fab fa-instagram"></i></a></li>
                         </ul>
                     </div>
@@ -134,19 +141,20 @@
                     <form class="account-widget-form">
                         <div class="row justify-content-center mb-25-none">
                             <div class="col-xl-12 form-group">
-                                <input type="text" class="form--control" placeholder="Your Name*">
+                                <input type="text" class="form--control" placeholder="Nombre*">
                             </div>
                             <div class="col-xl-12 form-group">
-                                <input type="email" class="form--control" placeholder="Your Email*">
+                                <input type="email" class="form--control" placeholder="Correo*">
                             </div>
                             <div class="col-xl-12 form-group">
-                                <input type="text" class="form--control" placeholder="Your Phone*">
+                                <input type="text" class="form--control" placeholder="Teléfono*">
                             </div>
                             <div class="col-xl-12 form-group">
-                                <textarea class="form--control" placeholder="Your Message*"></textarea>
+                                <textarea class="form--control" placeholder="Mensaje*"></textarea>
                             </div>
                             <div class="col-xl-12 form-group">
-                                <button type="submit" class="btn--base">Send Now <i class="fas fa-arrow-right ml-2"></i></button>
+                                <button type="submit" class="btn--base">Enviar<i
+                                        class="fas fa-arrow-right ml-2"></i></button>
                             </div>
                         </div>
                     </form>
@@ -168,7 +176,13 @@
         <div class="row justify-content-center mb-5-none">
             <div class="col-xl-12">
                 <div class="map-area">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.1899657893728!2d90.42380431666383!3d23.779746865573756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7499f257eab%3A0xe6b4b9eacea70f4a!2sManama+Tower!5e0!3m2!1sen!2sbd!4v1561542597668!5m2!1sen!2sbd" style="border:0" allowfullscreen></iframe>
+                    <!-- <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3070.1899657893728!2d90.42380431666383!3d23.779746865573756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7499f257eab%3A0xe6b4b9eacea70f4a!2sManama+Tower!5e0!3m2!1sen!2sbd!4v1561542597668!5m2!1sen!2sbd"
+                        style="border:0" allowfullscreen></iframe> -->
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.1855851662244!2d-109.71016168496729!3d23.090301219587413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af51532c7132c7%3A0xf09c9724da511bbd!2sBanana%20Marketing%20Group!5e0!3m2!1ses-419!2smx!4v1665783965303!5m2!1ses-419!2smx"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
