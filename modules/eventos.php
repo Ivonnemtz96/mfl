3c607572b9dcebede55922e233f494621cf06a68
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-16 16:58:29 -0700

    Cambios en eventos

--- modules/eventos.php
+++ modules/eventos.php
@@ -10,7 +10,7 @@
         <div class="row justify-content-center align-items-end mb-30-none">
             <div class="col-xl-12 col-lg-12 text-center mb-30">
                 <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
-                    <h1 class="title">Sobre <span>Nosotros</span></h1>
+                    <h1 class="title">Nuestros <span>Eventos</span></h1>
                 </div>
             </div>
         </div>
@@ -20,7 +20,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Mexican Fight League </li>
+            <li class="breadcrumb-item active" aria-current="page">Eventos</li>
         </ol>
     </nav>
 </div>
@@ -37,10 +37,14 @@
         <img src="assets/images/element/element-2.png" alt="element">
     </div>
     <div class="container">
-        <div class="row justify-content-center align-items-center mb-30-none">
+        <h2 class="section-title text-center mb-2">Eventos <span>Próximos</span></h2>
+        <h3 class="text-center mb-5">¡No te puedes perder de este emocionante evento!</h3>
+        <div class="row justify-content-between align-items-center mb-30-none">
             <div class="col-xl-6 col-lg-6 mb-30">
                 <div class="about-thumb">
-                    <img src="assets/images/about-2.png" alt="about">
+                    <a target="_blank" href="https://arema.mx/">
+                        <img src="assets/images/event/evento.jpg" alt="about">
+                    </a>
                     <!-- <div class="about-overlay-content">
                         <img src="assets/images/icon/icon-22.png" alt="icon">
                         <h4 class="title">NATIONAL 
@@ -50,87 +54,15 @@
                 </div>
             </div>
             <div class="col-xl-6 col-lg-6 mb-30">
-                <div class="about-content-area">
-                    <div class="section-header">
-                        <h2 class="section-title">Sobre <span>Mexican Fight League</span></h2>
-                        <p>Mexican Fight League es una empresa dedicada a la promoción y desarrollo del MMA en Mexico y
-                            Latinoamérica, contamos
-                            con basta experiencia en la producción de eventos en vivo para televisión habiendo trabajado
-                            ya con cadenas nacionales e internacionales como ESPN Knockout, combate SPACE, TDN, IMAGEN
-                            TV.
-                        </p>
-                        <p>
-                            Asimismo tenemos el personal, equipo de iluminación, pantallas (PQR) y unidad móvil propias
-                            (TVMAR) para garantizar la misma calidad de producción sin importar la sede del evento.
-                            <br><br>
-                            Se tiene programado realizar un evento mensual en diferentes ciudades dentro de la República
-                            mexicana iniciando en Noviembre del 2022, si fuera necesario estamos en posibilidad de hacer
-                            shows con mayor frecuencia.
-                            <br><br>
-                            MFL es sin duda la liga mejor estructurada en el país y con la plataforma adecuada pronto
-                            seremos la empresa referente en Latinoamérica.
-                        </p>
-                    </div>
+                <h4>Recuerda que los boletos ya están a la venta a través de arema.mx <br><br></h4>
+                <b>¡Dale clik a la imagen y consigue tus boletos!</b>
+                <div class="banner-arrow">
+                    <span>¡No te quedes sin el tuyo!</span> <img src="assets/images/element/element-10.png"
+                        alt="element">
                 </div>
-            </div>
-        </div>
-        <div class="row mt-5">
-            <div class="col-md-6">
-                <div class="about-content-area">
-                    <div class="section-header">
-                        <h2 class="section-title"><span>Misión</span></h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos necessitatibus provident distinctio? Error provident voluptatum facilis unde ad, molestias explicabo tempore, aut expedita nemo debitis, veniam repellat accusantium! Adipisci, ad.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="about-content-area">
-                    <div class="section-header">
-                        <h2 class="section-title">Visión</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto repellat, optio aspernatur placeat quasi reiciendis facilis vitae? Est cumque fuga perferendis, obcaecati beatae maiores dicta, voluptas, doloribus reiciendis veritatis nobis.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div hidden class="about-item-area about-item-area-two pt-120">
-            <div class="row justify-content-center mb-30-none">
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-30">
-                    <div class="about-item">
-                        <div class="about-icon">
-                            <img src="assets/images/icon/icon-23.png" alt="icon">
-                        </div>
-                        <div class="about-content">
-                            <h4 class="title">FREE FITNESS TRAINING</h4>
-                            <p>We teach martial arts because we want to Unlike other martial arts schools.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-30">
-                    <div class="about-item">
-                        <div class="about-icon">
-                            <img src="assets/images/icon/icon-24.png" alt="icon">
-                        </div>
-                        <div class="about-content">
-                            <h4 class="title">CARDIO AND STRENGTH</h4>
-                            <p>We teach martial arts because we want to Unlike other martial arts schools.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-30">
-                    <div class="about-item">
-                        <div class="about-icon">
-                            <img src="assets/images/icon/icon-25.png" alt="icon">
-                        </div>
-                        <div class="about-content">
-                            <h4 class="title">REGIONAL ASSOCIATIONS</h4>
-                            <p>We teach martial arts because we want to Unlike other martial arts schools.</p>
-                        </div>
-                    </div>
-                </div>
+                <a target="_blank" href="https://arema.mx/" style="margin-top: 2rem;">
+                    <img style="width: 90%" src="assets/images/arema.png" alt="">
+                </a>
             </div>
         </div>
     </div>
@@ -149,15 +81,15 @@
         <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-8 text-center">
                 <div class="section-header" data-aos="fade-up" data-aos-duration="1200">
-                    <h2 class="section-title">Nuestras <span>Reglas</span> </h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat delectus laudantium, commodi labore voluptatibus praesentium!
-                    </p>
+                    <h2 class="section-title">Eventos <span>Pasados</span> </h2>
                 </div>
             </div>
         </div>
         <div class="row justify-content-center align-items-center mb-30-none">
-            <div class="col-xl-8 col-lg-8 mb-30">
+            <div class="col-md-12">
+                <h2 class="section-title text-center"> <span>Próximamente</span> </h2>
+            </div>
+            <!-- <div class="col-xl-8 col-lg-8 mb-30">
                 <div class="feature-item-area">
                     <div class="row justify-content-center mb-60-none">
                         <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
@@ -229,7 +161,7 @@
                         <img src="assets/images/element/element-33.png" alt="element">
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
