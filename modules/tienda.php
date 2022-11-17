850db29b0c9218fb31fe64bb25147ed60b778f92
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-17 14:37:00 -0700

    tienda OK

---
+++ modules/tienda.php
@@ -0,0 +1,64 @@
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start Banner
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img"
+    data-background="assets/images/bg/bg-12.png">
+    <div class="section-logo-text">
+        <span class="title">MFL</span>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-end mb-30-none">
+            <div class="col-xl-12 col-lg-12 text-center mb-30">
+                <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
+                    <h1 class="title">Compra tus <span>boletos</span></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="breadcrumb-area">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Tienda</li>
+        </ol>
+    </nav>
+</div>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start About
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="about-section about-section--style ptb-120">
+    <div class="about-element-one my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
+        <img src="assets/images/element/element-1.png" alt="element">
+    </div>
+    <div class="about-element-two my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
+        <img src="assets/images/element/element-2.png" alt="element">
+    </div>
+    <div class="container">
+        <div class="section-header text-center" data-aos="fade-up" data-aos-duration="1200">
+            <h2 class="section-title">Compra tus <span>boletos</span> </h2>
+        </div>
+        <h3 class="text-center mb-5">¡No te quedes sin el tuyo!</h3>
+        <p class="text-center mb-5">Dale click a la siguiente imagen y consigue tus boletos antes de que se acaben y disfruta de lo mejor de Mexican Fight League</p>
+        <div class="row text-center align-items-center mb-30-none">
+            <div class="col-xl-12 col-lg-12 mb-30">
+                <div class="about-thumb">
+                    <a target="_blank" href="https://arema.mx/">
+                        <img src="assets/images/arema.png" alt="about">
+                    </a>
+                    <!-- <div class="about-overlay-content">
+                        <img src="assets/images/icon/icon-22.png" alt="icon">
+                        <h4 class="title">NATIONAL 
+                            BOXING
+                            AWARD</h4>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    End About
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
