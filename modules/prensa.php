71fa5ca1ff27936b90e95255be684d69f374f22c
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-17 16:11:22 -0700

    Prensa Ok

---
+++ modules/prensa.php
@@ -0,0 +1,124 @@
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start Banner
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img"
+    data-background="assets/images/bg/bg-12.png">
+    <div class="section-logo-text">
+        <span class="title">MFL</span>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-end mb-30-none">
+            <div class="col-xl-12 col-lg-12 text-center mb-30">
+                <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
+                    <h1 class="title"> Prensa</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="breadcrumb-area">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Prensa</li>
+        </ol>
+    </nav>
+</div>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start Feature
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="feature-section feature-section-two feature-section--style pt-120 pb-100">
+    <div class="feature-element">
+        <img src="assets/images/element/element-1.png" alt="element">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8 text-center">
+                <div class="section-header" data-aos="fade-up" data-aos-duration="1200">
+                    <h2 class="section-title"> <span>Prensa</span> </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center mb-30-none">
+            <div class="col-md-12">
+                <h2 class="section-title text-center"> <span>Próximamente</span> </h2>
+            </div>
+            <!-- <div class="col-xl-8 col-lg-8 mb-30">
+                <div class="feature-item-area">
+                    <div class="row justify-content-center mb-60-none">
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>01.</span> Regla 1</h4>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel reiciendis quas recusandae natus quibusdam maxime porro neque placeat nam?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>02.</span> Regla 2</h4>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit architecto ad assumenda ex perspiciatis quam!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>03.</span> Regla 03</h4>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem voluptatum quo aperiam, ducimus laboriosam nostrum.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>04.</span> Regla 04</h4>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique voluptas non totam. Repellat, necessitatibus quia?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 mb-30">
+                <div class="feature-thumb-area" data-aos="fade-left" data-aos-duration="1200">
+                    <div class="feature-thumb">
+                        <img src="assets/images/element/element-33.png" alt="element">
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</section>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    End Feature
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
