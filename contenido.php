87bf42dd1e7ba5c485c03582257dbc52f6013c6b
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-17 16:06:04 -0700

    ranquings OK con fotos y datos de peleadores, se crea contenidosphp

---
+++ contenido.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Eventos';
+$eventos = 'active';
+include('includes/head.php');
+?>
+
+<body >
+    <?php
+        include('includes/header2.php');
+        include('includes/preloader.php');
+        include('modules/contenido.php');
+        include('includes/footer.php');
+        include('includes/scripts.php');
+    ?>
+
+</body>
+
+</html>
---
+++ modules/contenido.php
@@ -0,0 +1,170 @@
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start Banner
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="banner-section banner-section-two inner-banner-section bg-overlay-red bg_img"
+    data-background="assets/images/bg/bg-12.png">
+    <div class="section-logo-text">
+        <span class="title">MFL</span>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-end mb-30-none">
+            <div class="col-xl-12 col-lg-12 text-center mb-30">
+                <div class="banner-content" data-aos="fade-up" data-aos-duration="1800">
+                    <h1 class="title">Nuestros <span>Eventos</span></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="breadcrumb-area">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Eventos</li>
+        </ol>
+    </nav>
+</div>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start About
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="about-section about-section--style ptb-120">
+    <div class="about-element-one my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
+        <img src="assets/images/element/element-1.png" alt="element">
+    </div>
+    <div class="about-element-two my-paroller" data-paroller-factor="0.2" data-paroller-type="foreground"
+        data-paroller-direction="vertical">
+        <img src="assets/images/element/element-2.png" alt="element">
+    </div>
+    <div class="container">
+        <h2 class="section-title text-center mb-2">Eventos <span>Próximos</span></h2>
+        <h3 class="text-center mb-5">¡No te puedes perder de este emocionante evento!</h3>
+        <div class="row justify-content-between align-items-center mb-30-none">
+            <div class="col-xl-6 col-lg-6 mb-30">
+                <div class="about-thumb">
+                    <a target="_blank" href="https://arema.mx/">
+                        <img src="assets/images/event/evento.jpg" alt="about">
+                    </a>
+                    <!-- <div class="about-overlay-content">
+                        <img src="assets/images/icon/icon-22.png" alt="icon">
+                        <h4 class="title">NATIONAL 
+                            BOXING
+                            AWARD</h4>
+                    </div> -->
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 mb-30">
+                <h4>Recuerda que los boletos ya están a la venta a través de arema.mx <br><br></h4>
+                <b>¡Dale clik a la imagen y consigue tus boletos!</b>
+                <div class="banner-arrow">
+                    <span>¡No te quedes sin el tuyo!</span> <img src="assets/images/element/element-10.png"
+                        alt="element">
+                </div>
+                <a target="_blank" href="https://arema.mx/" style="margin-top: 2rem;">
+                    <img style="width: 90%" src="assets/images/arema.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    End About
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Start Feature
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
+<section class="feature-section feature-section-two feature-section--style pt-120 pb-100">
+    <div class="feature-element">
+        <img src="assets/images/element/element-1.png" alt="element">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-8 text-center">
+                <div class="section-header" data-aos="fade-up" data-aos-duration="1200">
+                    <h2 class="section-title">Eventos <span>Pasados</span> </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center mb-30-none">
+            <div class="col-md-12">
+                <h2 class="section-title text-center"> <span>Próximamente</span> </h2>
+            </div>
+            <!-- <div class="col-xl-8 col-lg-8 mb-30">
+                <div class="feature-item-area">
+                    <div class="row justify-content-center mb-60-none">
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>01.</span> Regla 1</h4>
+                                    <p>
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vel reiciendis quas recusandae natus quibusdam maxime porro neque placeat nam?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>02.</span> Regla 2</h4>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit architecto ad assumenda ex perspiciatis quam!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>03.</span> Regla 03</h4>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem voluptatum quo aperiam, ducimus laboriosam nostrum.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6 mb-60">
+                            <div class="feature-item" data-aos="zoom-in" data-aos-duration="1200">
+                                <div class="feature-icon-area">
+                                    <div class="feature-icon">
+                                        <img src="assets/images/icon/reglas.png" alt="icon">
+                                    </div>
+                                </div>
+                                <div class="feature-content">
+                                    <h4 class="title"><span>04.</span> Regla 04</h4>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique voluptas non totam. Repellat, necessitatibus quia?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 mb-30">
+                <div class="feature-thumb-area" data-aos="fade-left" data-aos-duration="1200">
+                    <div class="feature-thumb">
+                        <img src="assets/images/element/element-33.png" alt="element">
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</section>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    End Feature
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->
